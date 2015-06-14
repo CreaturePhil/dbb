a6369189871572747d5dd425a9a8a9ad67806ef7
Phil <birkal@outlook.com>
2015-06-13 20:51:07 -0700

    Add getAll

--- test/db.js
+++ test/db.js
@@ -32,6 +32,17 @@ describe('Chidori#set', function() {
     expect(json['default']).to.be.an('object');
     expect(json['default'].key).to.equal('value');
   });  
+
+  it('should set a users', function() {
+    db('users').set('testuser1', {id: 1, name: 'Phil'});
+    db('users').set('testuser2', {id: 2, name: 'Jack'});
+    db('users').set('testuser3', {id: 3, name: 'Micheal'});
+    var json = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+    expect(json['users']).to.be.an('object');
+    expect(json['users'].testuser1).to.eql({id: 1, name: 'Phil'});
+    expect(json['users'].testuser2).to.eql({id: 2, name: 'Jack'});
+    expect(json['users'].testuser3).to.eql({id: 3, name: 'Micheal'});
+  });  
 });
 
 describe('Chidori#get', function() {
@@ -39,5 +50,23 @@ describe('Chidori#get', function() {
     var key = db().get('key');
     expect(key).to.be.a('string');
     expect(key).to.equal('value');
+  });
+});
+
+describe('Chidori#getAll', function() {
+  it('should get all the keys', function() {
+    var keys = db().getAll();
+    var users = db('users').getAll();
+    expect(keys).to.be.an('object');
+    expect(users).to.be.an('object');
+    expect(keys).to.be.eql({key: 'value'});
+    var data = {"testuser1":{"id":1,"name":"Phil"},"testuser2":{"id":2,"name":"Jack"},"testuser3":{"id":3,"name":"Micheal"}};
+    expect(users).to.be.eql(data);
+  });
+
+  after(function() {
+    fs.unlink('db.json', function(err) {
+      if (err) throw new Error(err);
+    });
   });
 });
